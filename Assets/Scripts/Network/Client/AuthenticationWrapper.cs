b0f155bcf9cd1c84bb421eda46cf204076c44299
xitxat <xitxat@outlook.com>
2024-10-06 20:47:42 +0200

    NetBootstrap UGS Auth OK!

---
+++ Assets/Scripts/Network/Client/AuthenticationWrapper.cs
@@ -0,0 +1,55 @@
+using System.Threading.Tasks;
+using Unity.Services.Authentication;
+using UnityEngine;
+
+
+// static: call from anywhere
+public static class AuthenticationWrapper
+{
+
+    public static AuthState AuthState { get; private set; } = AuthState.NotAuthenticated;
+
+    //  Authenticate Player via UGS & Client Game Manager returns T/F
+    public static async Task<AuthState> DoAuth(int maxTries = 5)
+    {
+        if(AuthState == AuthState.Authenticated)
+        {
+            return AuthState;
+        }
+
+        AuthState = AuthState.Authenticating;
+
+        int tries = 0;
+        while(AuthState == AuthState.Authenticating && tries < maxTries)
+        {
+            //  Straight in, no 3rd party auth, ie Google, FB
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+
+            if(AuthenticationService.Instance.IsSignedIn && AuthenticationService.Instance.IsAuthorized)
+            {
+                AuthState = AuthState.Authenticated;
+                break;
+            }
+
+            // On Fail
+            Debug.LogError("<color=orange>Retrying Authentication Service, ¦|</color>");
+
+            tries++;
+            await Task.Delay(1000);
+        }
+
+        return AuthState;
+    }
+
+}
+
+public enum AuthState
+{
+    NotAuthenticated,
+    Authenticating,
+    Authenticated,
+    TimeOut,
+    Error
+
+
+}
--- Assets/Scripts/Network/Client/ClientGameManager.cs
+++ Assets/Scripts/Network/Client/ClientGameManager.cs
@@ -1,11 +1,35 @@
 using System.Threading.Tasks;
+using Unity.Services.Core;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+       //   AUTHENTICATE PLAYER
 public class ClientGameManager 
 {
+    private const string MenuSceneName = "Menu";
+
+
     //  Async runs for as long as it needs
-       public async Task InitAsync()
+    //  Return bool for Authenticated
+    public async Task<bool> InitAsync()
     {
         //  Authentications (Task)
+        await UnityServices.InitializeAsync();
+
+        // Await the 5 max tries & return state
+        AuthState authState =  await AuthenticationWrapper.DoAuth();
+
+        if(authState == AuthState.Authenticated)
+        {
+            return true;
+        }
+
+        return false;
+
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene(MenuSceneName);
     }
 }
