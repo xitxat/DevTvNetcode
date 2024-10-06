55e6eeeadf33e2284ee9e18893b236d05f1d4e51
xitxat <xitxat@outlook.com>
2024-10-06 13:04:50 +0200

    Particle cloud OK

---
+++ Assets/Scripts/Utils/SpawnOnDestroy.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class SpawnOnDestroy : MonoBehaviour
+{
+
+    [SerializeField] private GameObject prefab;
+
+    private void OnDestroy()
+    {
+        Instantiate(prefab, transform.position, Quaternion.identity);
+    }
+}
