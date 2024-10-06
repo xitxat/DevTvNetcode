c9376609d1cdb49977a4e1faebc635b4c9e9f4b7
xitxat <xitxat@outlook.com>
2024-10-06 18:26:19 +0200

    async Task

---
+++ Assets/Scripts/Network/Client/ClientGameManager.cs
@@ -0,0 +1,11 @@
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class ClientGameManager 
+{
+    //  Async runs for as long as it needs
+       public async Task InitAsync()
+    {
+        //  Authentications (Task)
+    }
+}
