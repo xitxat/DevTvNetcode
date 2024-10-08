eac49aab757569f4787280fcf92582a2026fd65c
xitxat <xitxat@outlook.com>
2024-10-08 15:52:21 +0200

    NameSelector setup

---
+++ Assets/Scripts/UI/NameSelector.cs
@@ -0,0 +1,50 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NameSelector : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField nameField;
+    [SerializeField] private Button connectButton;
+    [SerializeField] private int minNameLength = 1;
+    [SerializeField] private int maxNameLength = 12;
+
+    const string PlayerNameKey = "PlayerName";
+
+
+    void Start()
+    {
+        //  If dedicated server [headless(no gfx)]: skip scene
+        if (SystemInfo.graphicsDeviceType == UnityEngine.Rendering.GraphicsDeviceType.Null)
+        {
+            // Set scene change dynamically. sc++
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            return;
+        }
+
+            // Will check if name is already in PlayerPrefs (registered from previous game)
+            // if T, set into txt field
+            nameField.text =  PlayerPrefs.GetString(PlayerNameKey, string.Empty);
+
+        HandleNameChange();
+    }
+
+    public void HandleNameChange()
+    {
+        connectButton.interactable =
+            nameField.text.Length >= minNameLength &&
+            nameField.text.Length <= maxNameLength;
+    }
+
+    public void Connect()
+    {
+        // Set Player Prefs & go to next scene
+        //  Get the players name
+        PlayerPrefs.SetString(PlayerNameKey, nameField.text);
+
+        // Set scene change dynamically. sc++
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
