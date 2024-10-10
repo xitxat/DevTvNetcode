0d0d8a36e291cc3ae1b0eef99589f431947902df
xitxat <xitxat@outlook.com>
2024-10-10 14:34:16 +0200

    re import NetworkClient

---
+++ Assets/Scripts/Network/Client/NetworkClient.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NetworkClient
+{
+
+    private NetworkManager networkManager;
+
+    private const string MenuSceneName = "Menu";
+
+
+    // constructor
+    public NetworkClient(NetworkManager networkManager)
+    {
+        this.networkManager = networkManager;
+
+        // triggered when Server is Connected to.[contains data]
+        networkManager.OnClientDisconnectCallback += OnClientDisconnect;
+    }
+
+
+
+    private void OnClientDisconnect(ulong clientId)
+    {
+        // Allows rejoin game without reopening app.
+        // Server shutdown sends clients back to Menu
+
+        // Check if we are a client that hosts a server
+        //   Don't disconnect
+        if (clientId != 0 && clientId != networkManager.LocalClientId) { return; }
+
+        // Change scene for client stranded in "GAME" upon an error, etc,
+        //  Go back to main menu
+        if (SceneManager.GetActiveScene().name != MenuSceneName)
+        {
+            SceneManager.LoadScene(MenuSceneName);
+        }
+
+        // If already on Main "MENU" & error / timeout
+        if (networkManager.IsConnectedClient)
+        {
+            networkManager.Shutdown();
+        }
+
+
+    }
+}
