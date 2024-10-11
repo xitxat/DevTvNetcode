7d910bbf265e6c6c5d680ced68993d8ffeef2613
xitxat <xitxat@outlook.com>
2024-10-11 17:22:08 +0200

    save

---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+    // SP list updated by spawnPoints in scene
+    // Host creates the SP's when scene loads. Host initial spawns in at .Zero
+public class SpawnPoint : MonoBehaviour
+{
+    private static List<SpawnPoint> spawnPoints = new List<SpawnPoint>();
+
+    // List Access  
+    // Static (instance not needed, just ret V3
+    public  static Vector3 GetRandomSpawnPos()
+    {
+        if(spawnPoints.Count == 0)
+        {
+            return Vector3.zero;
+        }
+
+        //  Get a random index between 0 (inclusive) and myList.Count (exclusive)
+        //int randomIndex = Random.Range(0, spawnPoints.Count);
+        //return  spawnPoints[randomIndex].transform.position;
+
+         return  spawnPoints[Random.Range(0, spawnPoints.Count)].transform.position;
+    }
+
+    public void OnEnable()
+    {
+        spawnPoints.Add(this);
+    }
+
+    public void OnDisable()
+    {
+        spawnPoints.Remove(this);
+    }
+
+
+    // Custom Editor scene view icon
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawSphere(transform.position, 1);
+    }
+
+}
