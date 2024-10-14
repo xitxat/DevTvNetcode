d8185edfde2302cf436b433417688357db0bd07b
xitxat <xitxat@outlook.com>
2024-10-14 14:58:23 +0200

    Spawn Bounty coin loop

--- Assets/Scripts/Coins/CoinWallet.cs
+++ Assets/Scripts/Coins/CoinWallet.cs
@@ -2,6 +2,8 @@ using System;
 using Unity.Netcode;
 using UnityEngine;
 
+//  https://www.gamedev.tv/dashboard/courses/22
+
 public class CoinWallet : NetworkBehaviour
 {
 
@@ -50,6 +52,19 @@ public class CoinWallet : NetworkBehaviour
         // Excess coin check
         if (bountyCoinValue < minBountyCoinValue) { return; }
 
+        // Spawn Bounty coin loop
+        for (int i = 0; i < bountyCoinCount; i++)
+        {
+            // Store first, to be able to set value
+           BountyCoin coinInstance = Instantiate(coinPrefab, GetSpawnPoint(), Quaternion.identity);
+            coinInstance.SetValue(bountyCoinValue);
+
+            // Spawned objects require NetworkObject component
+            //      Inherited from BASE Coin NetworkObject component
+            // No ID , no owner on Spawn
+            coinInstance.NetworkObject.Spawn();
+        }
+
     }
 
 
@@ -75,4 +90,22 @@ public class CoinWallet : NetworkBehaviour
         TotalCoins.Value -= costToFire;
     }
 
+    // Via dead player spread radius
+    private Vector2 GetSpawnPoint()
+    {
+
+        while (true)
+        {
+            // insideUnitCircle is 1 unit, cast
+            Vector2 spawnPoint = (Vector2)transform.position +
+                UnityEngine.Random.insideUnitCircle * coinSpread;
+
+            // Use Physics2D.OverlapCircle to check if the area is free
+            Collider2D hitCollider = Physics2D.OverlapCircle(spawnPoint, coinRadius, layerMask);
+            if (hitCollider == null)
+            {
+                return spawnPoint; // No colliders found, return spawn point
+            }
+        }
+    }
 }
