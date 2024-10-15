4008ef73d9ddb3ba1e5345d5b6227a74f8a321a7
xitxat <xitxat@outlook.com>
2024-10-15 14:52:33 +0200

    spawn mat

--- Assets/Scripts/Combat/HealingZone.cs
+++ Assets/Scripts/Combat/HealingZone.cs
@@ -3,6 +3,9 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 
+
+// https://www.gamedev.tv/dashboard/courses/22 4.14 , 4.15
+
 public class HealingZone : NetworkBehaviour
 
 {
@@ -17,8 +20,38 @@ public class HealingZone : NetworkBehaviour
     [SerializeField] int coinsPerTick = 10;
     [SerializeField] int healthPerTick = 10;
 
+    // Power of the healing bar. Clients Un/Sub to HealPower
+    private NetworkVariable<int> HealPower = new NetworkVariable<int>();
+
+
     private List<TankPlayer> playersInZone = new List<TankPlayer>();
 
+
+    public override void OnNetworkSpawn()
+    {
+        if (IsClient)
+        {
+            HealPower.OnValueChanged += HandleHealPowerChanged;
+        }
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (IsClient)
+        {
+            HealPower.OnValueChanged -= HandleHealPowerChanged;
+
+            // On server connect, mats may not have full charge.
+            // manual reset to update UI to Initial value
+            HandleHealPowerChanged(0, HealPower.Value);
+        }
+
+
+    }
+
+
+
+    #region LIST
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (!IsServer) { return; }
@@ -32,7 +65,7 @@ public class HealingZone : NetworkBehaviour
 
         playersInZone.Add(player);
 
-        Debug.Log($"<color=green>On Health mat: {player.PlayerName.Value}</color>");
+       // Debug.Log($"<color=green>On Health mat: {player.PlayerName.Value}</color>");
 
     }    
     
@@ -44,9 +77,16 @@ public class HealingZone : NetworkBehaviour
 
         playersInZone.Remove(player);
 
-        Debug.Log($"<color=green>Left Health mat: {player.PlayerName.Value}</color>");
+       // Debug.Log($"<color=green>Left Health mat: {player.PlayerName.Value}</color>");
 
     }
+    #endregion
 
 
+    private void HandleHealPowerChanged(int oldHealPower, int newHealPower)
+    {
+        // Update (float) Fill amt of hp bar
+        // only need to cast 1 of the ints
+        healPowerBar.fillAmount = (float)newHealPower / maxHealPower;
+    }
 }
