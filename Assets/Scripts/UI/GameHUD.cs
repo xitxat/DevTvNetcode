e10aa6b89f3d90d45599feb414d2d499b0dd695a
xitxat <xitxat@outlook.com>
2024-10-21 12:04:25 +0200

    HandleLobbyCodeChanged

--- Assets/Scripts/UI/GameHUD.cs
+++ Assets/Scripts/UI/GameHUD.cs
@@ -1,16 +1,49 @@
+using System;
 using TMPro;
+using Unity.Collections;
 using Unity.Netcode;
 using UnityEngine;
 
 // ADD TO GameHUD G.O.
 
 // Button: "Exit Arena" : ref this.LeaveGame
+// Access Network variables with .Value
 public class GameHUD : NetworkBehaviour
 {
     [SerializeField] private TMP_Text lobbyCodeText;
+
+    // TODO User Player name display on Menu scene
     //  [SerializeField] private TMP_Text ~Player name text; NameSelector , ClientGameManager
 
-    public void LeaveGame()
+    // Store Join Code text [init () with a default value ""]
+    private NetworkVariable<FixedString32Bytes> lobbyCode =
+        new NetworkVariable<FixedString32Bytes>("");
+
+    // GET JOIN CODE
+    public override void OnNetworkSpawn()
+    {
+        if (IsClient)
+        {
+            lobbyCode.OnValueChanged += HandleLobbyCodeChanged;
+            //Force sync, resync, update, refresh
+            HandleLobbyCodeChanged(string.Empty, lobbyCode.Value);
+        }
+
+        if (!IsHost) { return; }
+        lobbyCode.Value = HostSingleton.Instance.GameManager.JoinCode;
+    }
+
+
+    public override void OnNetworkDespawn()
+    {
+        if (IsClient)
+        {
+            lobbyCode.OnValueChanged -= HandleLobbyCodeChanged;
+        }
+    }
+
+
+        public void LeaveGame()
     {
         // For Self Hosting
         if (NetworkManager.Singleton.IsHost)
@@ -21,4 +54,10 @@ public class GameHUD : NetworkBehaviour
         // For Client wanting to Leave
         ClientSingleton.Instance.GameManager.Disconnect();
     }
+
+    private void HandleLobbyCodeChanged(FixedString32Bytes oldCode, FixedString32Bytes newCode)
+    {
+        lobbyCodeText.text = newCode.ToString();
+    }
+
 }
