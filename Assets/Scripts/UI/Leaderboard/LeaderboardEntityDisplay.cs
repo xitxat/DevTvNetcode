b597ca6688a5a4fac3b31da0ce922d4a7454b9c6
xitxat <xitxat@outlook.com>
2024-10-22 20:56:22 +0200

    Player & Team text colours

--- Assets/Scripts/UI/Leaderboard/LeaderboardEntityDisplay.cs
+++ Assets/Scripts/UI/Leaderboard/LeaderboardEntityDisplay.cs
@@ -9,29 +9,36 @@ public class LeaderboardEntityDisplay : MonoBehaviour
 {
 
     // Exposed for Leaderboard
+    public int TeamIndex { get; private set; } //br6.06
     public ulong ClientId { get; private set; }
     public int Coins { get; private set; }
 
 
     [SerializeField] private TMP_Text displayText;
-    [SerializeField] private Color myColor;
-    FixedString32Bytes playerName;
+    FixedString32Bytes displayName;
 
 
     //  Custom Initialise not Unity's InitialiZe
-    public void Initialise(ulong clientId, FixedString32Bytes playerName, int coins)
+    public void Initialise(ulong clientId, FixedString32Bytes displayName, int coins)
     {
         ClientId = clientId; // static
-        this.playerName = playerName;
-
-        // If Me (colourise name)
-        // Default Font Face HDR color in LeaderBoardEntity prefab. 
-        if (clientId == NetworkManager.Singleton.LocalClientId)
-        {
-            displayText.color = myColor;
-        }
+        this.displayName = displayName;
 
         UpdateCoins(coins); // Dynamic update()
+    }
+
+    public void Initialise(int teamIndex, FixedString32Bytes displayName, int coins)
+    {
+        TeamIndex = teamIndex;
+        this.displayName = displayName;
+
+        UpdateCoins(coins); // Dynamic update()
+    }
+
+    // Player & Team text colours
+    public void SetColour(Color colour)
+    {
+        displayText.color = colour;
     }
 
     public void UpdateCoins(int coins)
@@ -44,7 +51,7 @@ public class LeaderboardEntityDisplay : MonoBehaviour
     public void UpdateText()
     {
         // siblinIndex Array from HandleLeaderboardEntitiesChanged()
-        displayText.text = $"{transform.GetSiblingIndex() + 1}: {playerName} [{Coins}]";
+        displayText.text = $"{transform.GetSiblingIndex() + 1}: {displayName} [{Coins}]";
         
     }
 
