49c1fb310aa01a494658e1c26d90e6149987b32a
xitxat <xitxat@outlook.com>
2024-10-22 20:34:23 +0200

    tp

--- Assets/Scripts/Player/TankPlayer.cs
+++ Assets/Scripts/Player/TankPlayer.cs
@@ -11,7 +11,7 @@ using UnityEngine;
 public class TankPlayer : NetworkBehaviour
 {
     //  Sync names, Team index[colours]. Cant sync normal strings.  .VALUE
-    public NetworkVariable<FixedString32Bytes> PlayerName = new NetworkVariable<FixedString32Bytes>("¦|");
+    public NetworkVariable<FixedString32Bytes> PlayerName = new NetworkVariable<FixedString32Bytes>(new FixedString32Bytes(""));
     public NetworkVariable<int> TeamIndex = new NetworkVariable<int>(0);
 
 
