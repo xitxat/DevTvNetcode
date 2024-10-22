b572a7677feecbc1f67e763514a0960881016680
xitxat <xitxat@outlook.com>
2024-10-22 13:18:46 +0200

    DestroySelfOnContact

--- Assets/Scripts/Utils/DestroySelfOnContact.cs
+++ Assets/Scripts/Utils/DestroySelfOnContact.cs
@@ -2,9 +2,15 @@ using UnityEngine;
 
 public class DestroySelfOnContact : MonoBehaviour
 {
-
+    [SerializeField] private Projectile projectile;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // Friendly Fire check
+        if (collision.TryGetComponent<TankPlayer>(out TankPlayer player))
+        {
+            if (player.TeamIndex.Value == projectile.TeamIndex) { return; }
+        }
+
         Destroy(gameObject);
     }
 }
